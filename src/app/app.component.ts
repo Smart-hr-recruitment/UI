eaa5f276f81cca966b555437457061837661fc0b
adamyanlina <adamyanlina97@gmail.com>
2021-05-08 19:06:29 +0400

    init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,93 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { MsalService } from '@azure/msal-angular';
+import { AuthenticationResult } from '@azure/msal-browser';
+import { OAuthSettings } from 'src/oauth';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'UI';
+
+  apiResponse: string;
+
+  constructor(private msalService: MsalService, private httpCLient: HttpClient) {
+    
+  }
+  ngOnInit(): void {
+    this.msalService.instance.handleRedirectPromise().then( res => {
+      if (res != null && res.account != null) {
+        this.msalService.instance.setActiveAccount(res.account)
+      }
+    });
+  }
+
+  isLoggedIn(): boolean {
+    return this.msalService.instance.getActiveAccount() != null;
+  }
+
+  login() {
+    // this.msalService.loginRedirect();
+    this.msalService.loginPopup()
+      .subscribe((response: AuthenticationResult) => {
+        this.msalService.instance.setActiveAccount(response.account);
+      });
+  }
+
+  logout() {
+    this.msalService.logout();
+  }
+  
+  getName(): string {
+    if(this.msalService.instance.getActiveAccount() == null) {
+      return 'Unknown';
+    }
+    return this.msalService.instance.getActiveAccount().name;
+  }
+
+  getUsers() {
+    this.httpCLient.get('https://graph.microsoft.com/v1.0/users').subscribe(res => {
+      this.apiResponse = JSON.stringify(res);
+    });
+  }
+
+  getSubordinates() {
+    this.httpCLient.get('https://graph.microsoft.com/v1.0/me/directReports').subscribe(res => {
+      this.apiResponse = JSON.stringify(res);
+    });
+  }
+
+  callProfile() {
+    this.httpCLient.get('https://graph.microsoft.com/v1.0/me').subscribe(res => {
+      this.apiResponse = JSON.stringify(res);
+    });
+  }
+
+  callMySkills() {
+    this.httpCLient.get('https://graph.microsoft.com/v1.0/me/?$select=displayName,skills').subscribe(res => {
+      this.apiResponse = JSON.stringify(res);
+    });
+  }
+
+  callMessages() {
+    this.httpCLient.get('https://graph.microsoft.com/v1.0/me/messages').subscribe(res => {
+      this.apiResponse = JSON.stringify(res);
+    });
+  }
+
+  callJoinedTeams() {
+    this.httpCLient.get('https://graph.microsoft.com/v1.0/me/joinedTeams').subscribe(res => {
+      this.apiResponse = JSON.stringify(res);
+    });
+  }
+
+  getBackendsApi() {
+    this.httpCLient.get('http://localhost:8000/api/v1/smarthr').subscribe(res => {
+      this.apiResponse = JSON.stringify(res);
+    });
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,64 @@
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { MsalInterceptor, MsalModule, MsalService, MSAL_GUARD_CONFIG, MSAL_INSTANCE, MsalInterceptorConfiguration, MSAL_INTERCEPTOR_CONFIG } from '@azure/msal-angular';
+import { InteractionType, IPublicClientApplication, PublicClientApplication } from '@azure/msal-browser';
+
+import { OAuthSettings } from '../oauth';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PublicPageComponent } from './public-page/public-page.component';
+import { RestrictedPageComponent } from './restricted-page/restricted-page.component';
+
+export function MSALInstanceFactory(): IPublicClientApplication {
+  return new PublicClientApplication({
+    auth: {
+      clientId: OAuthSettings.appId,
+      redirectUri: OAuthSettings.redirectUri
+    }
+  });
+}
+
+export function MSALInterceptorConfigFactory(): MsalInterceptorConfiguration {
+  const protectedResourceMap = new Map<string, Array<string>>();
+  protectedResourceMap.set('https://graph.microsoft.com/v1.0/me', OAuthSettings.scopes);
+  
+  protectedResourceMap.set('https://graph.microsoft.com/v1.0/users', OAuthSettings.scopes);
+  protectedResourceMap.set('http://localhost:8000/api/v1.0', ['api://17b39df8-f88c-4c65-9fc0-50e1b1bc3f39/api']);
+
+  return {
+    interactionType: InteractionType.Popup,
+    protectedResourceMap
+  };
+}
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PublicPageComponent,
+    RestrictedPageComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MsalModule,
+    HttpClientModule
   ],
-  providers: [],
+  providers: [
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: MsalInterceptor,
+      multi: true
+    }, {
+      provide: MSAL_INSTANCE,
+      useFactory: MSALInstanceFactory
+    }, {
+      provide: MSAL_INTERCEPTOR_CONFIG,
+      useFactory: MSALInterceptorConfigFactory
+    },
+    MsalService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
